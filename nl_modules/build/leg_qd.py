2aa624e5087a75770733199ec56b609e1b79cbd1
Nicky Liu <chi@nickyliu.com>
2025-05-24 00:36:53 +0800

    clean up

--- nl_modules/build/leg_qd.py
+++ nl_modules/build/leg_qd.py
@@ -122,10 +122,7 @@ class LegQd(RigModule):
             pf=rID,
             scale=xDr * rSz / 2,
         )
-        scale = (rSz * 1.5, rSz * 0.5, rSz * 2)
-        self.ikc = CurveNode(
-            "ikc", pf=rID, shape="trapezoid", scale=rSz, move=(0, 0, rSz * 4)
-        )
+        self.ikc = CurveNode("ikc", pf=rID, shape="trapezoid", scale=rSz)
         self.pvc = CurveNode("pvc", pf=rID, shape="locator", scale=rSz)
         self.smart_ctl = CurveNode("smart_ctl", pf=rID, shape="roll", scale=rSz / 2)
 
