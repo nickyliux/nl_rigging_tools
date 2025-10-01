e9fde9a8bb61d630d857b7a5e922b429b44697f9
Nicky Liu <chi@nickyliu.com>
2025-10-01 18:20:16 +0800

    Refactor corrSysSetup and corrJntSetup functions: streamline corrective joint system creation, enhance parameter handling, and improve clarity in joint setup logic.

--- nl_modules/utils/helper.py
+++ nl_modules/utils/helper.py
@@ -8,10 +8,10 @@ from nl_modules.nodel.jnt_node import JntNode
 from nl_modules.utils.color import Color
 
 
-def corrJntSetup(
-    tgtJnt=None, parentJnt=None, rotator=None, driver="rz", driven="ty", dir=1
+def corrSysSetup(
+    tgtJnt=None, parentJnt=None, rotator=None, buildTy=True, buildTz=False
 ):
-
+    """Create a corrective joint system that responds to the rotation of a driver object."""
     tgtJnt = DagNode(tgtJnt)
     parentJnt = DagNode(parentJnt)
     rotator = DagNode(rotator)
@@ -20,19 +20,31 @@ def corrJntSetup(
         mc.warning("Input not found.")
         return
 
-    jnt_rad = tgtJnt.a.radius.get() / 2
-    grp = GrpNode("grp_#", pf="corr")
+    corrGrp = GrpNode("grp_#", pf="corr")
+    if buildTy:
+        corrJntSetup(tgtJnt, parentJnt, rotator, p=corrGrp)
+        corrJntSetup(tgtJnt, parentJnt, rotator, p=corrGrp, dir=-1)
+    if buildTz:
+        corrJntSetup(tgtJnt, parentJnt, rotator, driver="ry", driven="tz", p=corrGrp)
+        corrJntSetup(
+            tgtJnt, parentJnt, rotator, driver="ry", driven="tz", p=corrGrp, dir=-1
+        )
 
+
+def corrJntSetup(
+    tgtJnt=None, parentJnt=None, rotator=None, driver="rz", driven="ty", dir=1, p=None
+):
+    """Create a corrective joint setup that responds to the rotation of a driver object."""
     # ---------------------------------------------
-    grp2 = GrpNode("grp2_#", p=grp, pf="corr")
-    loc = LocNode("loc_#", p=grp2, pf="corr")
-    jnt = JntNode("jnt_#", p=loc, pf="corr", r=jnt_rad, color=Color.YELLOW)
+    grp = GrpNode("corr_grp_#", p=p, pf=tgtJnt)
+    jnt_rad = tgtJnt.a.radius.get()
+    jnt = JntNode("corr_#", p=grp, pf=tgtJnt, r=jnt_rad, color=Color.YELLOW)
 
-    oriCst = common.cstMulti(parentJnt, tgtJnt, grp2, cstType="ori")
-    tgtJnt.cstPoi(grp2)
+    common.cstMulti(parentJnt, tgtJnt, grp, cstType="ori")
+    tgtJnt.cstPoi(grp)
 
     dir = 1 if dir > 0 else -1
-    grp2.a.s.set(dir, dir, dir)
+    grp.a.s.set(dir, dir, dir)
 
     # offset but keep corrective in the middle -------------
     # ofsInitRota = jnt.a.add("ofsInitRota")
@@ -41,11 +53,13 @@ def corrJntSetup(
     # ------------------------------------------------------
 
     ofsInit = jnt.a.add("ofsInit", dv=1, min=0)
-    ofsScalePos = jnt.a.add("ofsScalePos", dv=1, min=0)
-    ofsScaleNeg = jnt.a.add("ofsScaleNeg", dv=1, min=0)
+    ofsScalePos = jnt.a.add("ofsScalePos", dv=2, min=0)
+    ofsScaleNeg = jnt.a.add("ofsScaleNeg", dv=2, min=0)
 
     r = rotator.a[driver]
     rAbs = (r >= 0).setCdn(ifTrue=r, ifFalse=r * -1)
 
     ofsScale = (r > 0).setCdn(ifTrue=ofsScalePos, ifFalse=ofsScaleNeg)
     ofsInit + ofsScale * rAbs / 90 >> jnt.a[driven]
+
+    jnt.a.showAttr()
