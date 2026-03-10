ed5d6288ece37fb49c725d5a285c032d33060f93
Nicky Liu <chi@nickyliu.com>
2026-03-10 19:40:48 +0800

    update About data

--- startUp.py
+++ startUp.py
@@ -6,13 +6,34 @@ from PySide2 import QtWidgets
 def showAbout(self):
     """Show the about dialog."""
 
-    aboutText = (
-        "NL Rigging Tools (NLRT) v2026.01.21\n\n"
-        "A modular auto rigging tool\n\n"
-        "Developed by Nicky Liu.\n"
-        "GitHub: https://github.com/nl/nl_rigging_tools\n"
-    )
-    QtWidgets.QMessageBox.about(None, "About", aboutText)
+    aboutText = """
+nl Rigging Tools ( nlRT ) version: v2026.03.10
+
+nlRT is under the terms of the MIT License
+
+Copyright (c) 2026 Nicky Liu
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+
+GitHub: https://github.com/nl/nl_rigging_tools
+    """
+    mc.confirmDialog(title="About nlRT", message=aboutText, button=["OK"])
 
 
 def createMenu():
@@ -25,8 +46,9 @@ def createMenu():
     if mc.menu("nlRT", exists=1, parent=melGlobal):
         print(">>>>>>>>> nlRT Menu Already Exists >>>>>>>>>")
     else:
-        nlMenu = mc.menu("nlRT", parent=melGlobal)
-        mc.menuItem(label="nl Rigging Tools", command=runCmd, parent=nlMenu)
+        nlMenu = mc.menu("nlRT", parent=melGlobal, to=True)
+        mc.menuItem(label="Open UI", command=runCmd, parent=nlMenu)
+        mc.menuItem(label="Close UI", command=closeCmd, parent=nlMenu)
+        mc.menuItem(divider=True, parent=nlMenu)
         mc.menuItem(label="About", command=showAbout, parent=nlMenu)
-        mc.menuItem(label="Close", command=closeCmd, parent=nlMenu)
         print(">>>>>>>>> nlRT Menu Loaded >>>>>>>>>")
